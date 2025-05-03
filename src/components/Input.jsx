fcb1f037865d1cd5bbd54215dadbc46339b0ac0b
acasasis-dev <angelito.casasis@gmail.com>
2025-05-03 23:44:04 +0800

    Dynamic tailwind styling for the inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,50 +1,14 @@
-import styled from "styled-components";
-
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => $invalid ? '#f87171': '#6b7280'};
-`
-
-const Input = styled.input`
-  ${({ $invalid }) => {
-		let color = '#374151'
-		let borderColor = 'transparent'
-		let backgroundColor = '#d1d5db'
-
-
-		if($invalid) {
-			color = '#ef4444'
-			borderColor = '#f73f3f'
-			backgroundColor = '#fed2d2'
-		}
-
-		return (`
-			width: 100%;
-			padding: 0.75rem 1rem;
-			line-height: 1.5;
-			background-color: ${ backgroundColor };
-			color: ${ color };
-			border: 1px solid ${ borderColor };
-			border-radius: 0.25rem;
-			box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-		`)
-  }}
-`
-
 export default function CustomInput({label, $invalid, ...props}) {
+	const labelClassColor = $invalid? 'text-red-400': 'text-stone-300'
+	const labelClass = `block mb-2 text-xs font-bold tracking-wide uppercase ${labelClassColor}`
+
+	const inputClassColor = $invalid? 'text-red-500 bg-red-100 border-red-300': 'text-gray-700 bg-stone-300'
+	const inputClass = `w-full px-3 py-2 leading-tight border rounded shadow ${inputClassColor}`
+	
 	return (
 		<p>
-			<Label $invalid={ $invalid }>{label}</Label>
-			<Input
-				$invalid={ $invalid }
-				{ ...props }
-      />
+			<label className={ labelClass }>{label}</label>
+			<input className={ inputClass } { ...props } />
     </p>
 	)
 }
